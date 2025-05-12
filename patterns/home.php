1f8a32affbcc42ed1be3a49c6b3d3552c116c119
hanneslsm <HannesLeismann@gmail.com>
2025-05-12 16:05:11 +0200

    Update templates

--- patterns/home.php
+++ patterns/home.php
@@ -5,11 +5,7 @@
  * Inserter: no
  */
 ?>
-<!-- wp:group {"align":"wide","style":{"dimensions":{"minHeight":"100dvh"}},"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"space-between","justifyContent":"stretch"}} -->
-<div class="wp-block-group alignwide" style="min-height:100dvh"><!-- wp:group {"align":"wide","layout":{"type":"default"}} -->
-<div class="wp-block-group alignwide"><!-- wp:group {"align":"wide","className":"is-style-section-base-2","style":{"spacing":{"padding":{"bottom":"var:preset|spacing|40","top":"var:preset|spacing|10"}},"border":{"bottom":{"color":"var:preset|color|base-3","width":"1px"},"top":[],"right":[],"left":[]}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignwide is-style-section-base-2" style="border-bottom-color:var(--wp--preset--color--base-3);border-bottom-width:1px;padding-top:var(--wp--preset--spacing--10);padding-bottom:var(--wp--preset--spacing--40)"><!-- wp:template-part {"slug":"header","area":"header","align":"wide"} /--></div>
-<!-- /wp:group -->
+<!-- wp:template-part {"slug":"header","area":"header","align":"wide"} /-->
 
 <!-- wp:group {"tagName":"main","metadata":{"name":"Main"},"style":{"spacing":{"margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}},"dimensions":{"minHeight":"100%"}},"layout":{"type":"constrained"}} -->
 <main class="wp-block-group" style="min-height:100%;margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40)"><!-- wp:query {"queryId":0,"query":{"perPage":10,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true},"align":"wide","layout":{"type":"default"}} -->
@@ -51,8 +47,6 @@
 <!-- /wp:paragraph -->
 <!-- /wp:query-no-results --></div>
 <!-- /wp:query --></main>
-<!-- /wp:group --></div>
 <!-- /wp:group -->
 
-<!-- wp:template-part {"slug":"footer","tagName":"footer"} /--></div>
-<!-- /wp:group -->
+<!-- wp:template-part {"slug":"footer","tagName":"footer"} /-->

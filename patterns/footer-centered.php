c94ab6c5747f2a96be164bf0c17b64a8a435e928
hanneslsm <HannesLeismann@gmail.com>
2025-05-12 15:52:53 +0200

    Update image to site-logo

--- patterns/footer-centered.php
+++ patterns/footer-centered.php
@@ -8,9 +8,7 @@
 ?>
 <!-- wp:group {"className":"is-style-footer","style":{"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}}},"layout":{"inherit":true,"type":"constrained"}} -->
 <div class="wp-block-group is-style-footer" style="padding-top:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30)"><!-- wp:group {"layout":{"type":"default"}} -->
-<div class="wp-block-group"><!-- wp:image {"lightbox":{"enabled":false},"width":"100px","sizeSlug":"full","linkDestination":"none","align":"center","className":"activate-rainbow","style":{"border":{"radius":"0px"}}} -->
-<figure class="wp-block-image aligncenter size-full is-resized has-custom-border activate-rainbow"><img src="http://upa25.local/wp-content/themes/upa25/assets/images/logo.png" alt="" style="border-radius:0px;width:100px"/></figure>
-<!-- /wp:image -->
+<div class="wp-block-group"><!-- wp:site-logo {"width":80,"align":"center"} /-->
 
 <!-- wp:paragraph {"align":"center"} -->
 <p class="has-text-align-center">Enter a short description of your company here. This helps your visitors to understand what you do and improves your ranking in search engines. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>

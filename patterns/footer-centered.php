5e867cd08115016f4ab51d8d58d24c3684600624
hanneslsm <HannesLeismann@gmail.com>
2025-05-12 15:24:14 +0200

    feat: enqueue footer and add footer centered

---
+++ patterns/footer-centered.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Title: Footer centered
+ * Slug: upa25/footer-centered
+ * Categories: footer
+ * Block Types: core/template-part/footer
+ */
+?>
+<!-- wp:group {"className":"is-style-footer","style":{"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}}},"layout":{"inherit":true,"type":"constrained"}} -->
+<div class="wp-block-group is-style-footer" style="padding-top:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30)"><!-- wp:group {"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:image {"lightbox":{"enabled":false},"width":"100px","sizeSlug":"full","linkDestination":"none","align":"center","className":"activate-rainbow","style":{"border":{"radius":"0px"}}} -->
+<figure class="wp-block-image aligncenter size-full is-resized has-custom-border activate-rainbow"><img src="http://upa25.local/wp-content/themes/upa25/assets/images/logo.png" alt="" style="border-radius:0px;width:100px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">Enter a short description of your company here. This helps your visitors to understand what you do and improves your ranking in search engines. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center"><em>Your Name</em><br>Your address<br>Contact info</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:navigation {"ref":26,"overlayMenu":"never","layout":{"type":"flex","justifyContent":"center"}} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->

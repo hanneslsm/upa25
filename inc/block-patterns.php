d6aa0c9dc51bab99304ec840bd0b04b5cab57013
hanneslsm <HannesLeismann@gmail.com>
2025-05-21 15:37:35 +0200

    Add FAQ section & category

--- inc/block-patterns.php
+++ inc/block-patterns.php
@@ -15,7 +15,7 @@ add_action('after_setup_theme', 'upa25_remove_core_patterns');
 
 function upa25_remove_core_patterns()
 {
-    remove_theme_support('core-block-patterns');
+	remove_theme_support('core-block-patterns');
 }
 
 /**
@@ -30,17 +30,21 @@ add_filter('should_load_remote_block_patterns', '__return_false');
  * @link https://developer.wordpress.org/themes/patterns/registering-patterns/#registering-a-pattern-category
  */
 
-add_action( 'init', 'upa25_register_pattern_categories', 1 );
+add_action('init', 'upa25_register_pattern_categories', 1);
 // add_action( 'after_setup_theme', 'upa25_register_pattern_categories', 5 );
 
-function upa25_register_pattern_categories() {
-    register_block_pattern_category( 'upa25/content', [
-        'label'       => __( 'Content', 'upa25' ),
-        'description' => __( 'Generic content layouts.', 'upa25' ),
-    ] );
-    register_block_pattern_category( 'upa25/cards', [
-        'label'       => __( 'Cards', 'upa25' ),
-        'description' => __( 'Generic card layouts.', 'upa25' ),
-    ] );
+function upa25_register_pattern_categories()
+{
+	register_block_pattern_category('upa25/content', [
+		'label'       => __('Content', 'upa25'),
+		'description' => __('Generic content layouts.', 'upa25'),
+	]);
+	register_block_pattern_category('upa25/cards', [
+		'label'       => __('Cards', 'upa25'),
+		'description' => __('Generic card layouts.', 'upa25'),
+	]);
+	register_block_pattern_category('upa25/faq', [
+		'label'       => __('FAQ', 'upa25'),
+		'description' => __('Layout for the FAQ Section.', 'upa25'),
+	]);
 }
-
---
+++ patterns/faq-01.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Title: FAQ-01
+ * Slug: upa25/faq-01
+ * Categories: upa25/faq
+ * Viewport width: 1280
+ * Description: FAQ section
+ */
+?>
+<!-- wp:group {"metadata":{"name":"Section"},"align":"full","className":"is-style-default","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull is-style-default"><!-- wp:group {"align":"wide","layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide"><!-- wp:spacer {"height":"var:preset|spacing|40"} -->
+<div style="height:var(--wp--preset--spacing--40)" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"center","width":"","style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|40","right":"var:preset|spacing|40"}}}} -->
+<div class="wp-block-column is-vertically-aligned-center" style="padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)"><!-- wp:heading -->
+<h2 class="wp-block-heading"><?php esc_html_e('Place your medium headline here.', 'upa25');?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><?php esc_html_e('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'upa25');?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:details {"className":"is-style-chevron"} -->
+<details class="wp-block-details is-style-chevron"><summary>Consectetur adipiscing elit?</summary><!-- wp:paragraph -->
+<p><?php esc_html_e('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'upa25');?></p>
+<!-- /wp:paragraph --></details>
+<!-- /wp:details -->
+
+<!-- wp:separator -->
+<hr class="wp-block-separator has-alpha-channel-opacity"/>
+<!-- /wp:separator -->
+
+<!-- wp:details {"className":"is-style-chevron"} -->
+<details class="wp-block-details is-style-chevron"><summary>Sed do eiusmod tempor?</summary><!-- wp:paragraph {"placeholder":"Type / to add a hidden block"} -->
+<p><?php esc_html_e('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'upa25');?></p>
+<!-- /wp:paragraph --></details>
+<!-- /wp:details -->
+
+<!-- wp:separator -->
+<hr class="wp-block-separator has-alpha-channel-opacity"/>
+<!-- /wp:separator -->
+
+<!-- wp:details {"className":"is-style-chevron"} -->
+<details class="wp-block-details is-style-chevron"><summary>Incididunt ut labore et dolore?</summary><!-- wp:paragraph -->
+<p><?php esc_html_e('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'upa25');?></p>
+<!-- /wp:paragraph --></details>
+<!-- /wp:details -->
+
+<!-- wp:separator -->
+<hr class="wp-block-separator has-alpha-channel-opacity"/>
+<!-- /wp:separator -->
+
+<!-- wp:details {"className":"is-style-chevron"} -->
+<details class="wp-block-details is-style-chevron"><summary>Quis nostrud exercitation?</summary><!-- wp:paragraph {"placeholder":"Type / to add a hidden block"} -->
+<p><?php esc_html_e('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'upa25');?></p>
+<!-- /wp:paragraph --></details>
+<!-- /wp:details --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"33.33%"} -->
+<div class="wp-block-column" style="flex-basis:33.33%"><!-- wp:image {"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/placeholder-3.webp" alt=""/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:spacer {"height":"var:preset|spacing|40"} -->
+<div style="height:var(--wp--preset--spacing--40)" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->

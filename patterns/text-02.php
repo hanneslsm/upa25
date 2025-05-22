93a94d424b0485ddcbc42965b0b7ab304fe21a19
hanneslsm <HannesLeismann@gmail.com>
2025-05-22 14:50:11 +0200

    Add Text patterns

---
+++ patterns/text-02.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Title: Text-02
+ * Slug: upa25/text-02
+ * Categories: Text
+ */
+?>
+<!-- wp:group {"metadata":{"name":"Section","categories":["upa25/content"],"patternName":"upa25/content-1"},"align":"full","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull"><!-- wp:group {"align":"wide","layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide"><!-- wp:spacer {"height":"var:preset|spacing|40"} -->
+<div style="height:var(--wp--preset--spacing--40)" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:heading -->
+<h2 class="wp-block-heading"><?php esc_html_e('Place your medium headline here.', 'upa25');?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:paragraph -->
+<p><?php esc_html_e('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque id odio mollis, bibendum nibh eget, lacinia nibh. In molestie at dui et tincidunt. In eu nulla eu nisl aliquet porta sed a urna. Nunc non facilisis augue, eget vulputate purus. Proin sed quam semper erat suscipit ornare. Nulla facilisis mattis felis, eget accumsan dolor luctus at. Donec augue arcu, sollicitudin et nulla et, porttitor imperdiet elit. Maecenas volutpat cursus feugiat. Sed et sapien ac leo semper sagittis. Donec eros tellus, dignissim sit amet dolor sit amet, euismod dapibus orci. Donec condimentum, leo vitae placerat egestas, felis lorem lacinia dui, eu gravida magna urna vitae elit. Nulla id nibh libero. Pellentesque vulputate est at leo ultrices, a eleifend mi posuere.', 'upa25');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:paragraph -->
+<p><?php esc_html_e('Sed placerat, odio in condimentum lacinia, augue justo ultricies lacus, in porta lectus leo euismod ipsum. Curabitur pretium, leo vel porttitor dictum, dolor metus congue metus, non hendrerit metus tortor sit amet purus. Vivamus dapibus vestibulum porta. Maecenas non scelerisque metus. Duis in purus sit amet augue auctor pulvinar. Phasellus auctor venenatis consectetur. Maecenas mattis hendrerit quam feugiat porta. Curabitur quis volutpat orci, eget facilisis libero. Cras elementum orci vel pharetra tincidunt. Proin at risus vel nunc mollis tincidunt. Proin ultricies blandit mauris quis sodales. Nulla commodo mauris at neque egestas, id efficitur neque dictum. Aenean nec iaculis sem, eget condimentum urna.', 'upa25');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:spacer {"height":"var:preset|spacing|40"} -->
+<div style="height:var(--wp--preset--spacing--40)" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->

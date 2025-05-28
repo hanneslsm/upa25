6aa6efd836153de770cd49d7279cf7d0823a9084
hanneslsm <HannesLeismann@gmail.com>
2025-05-28 22:32:16 +0200

    Update avatar component

--- patterns/component-avatar-stack.php
+++ patterns/component-avatar-stack.php
@@ -6,29 +6,28 @@
  * Viewport width: 320
  */
 ?>
-<!-- wp:group {"metadata":{"name":"Component"},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<!-- wp:group {"metadata":{"name":"Component","categories":["upa25/components"],"patternName":"upa25/component-avatar-stack"},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
 <div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
 <div class="wp-block-group"><!-- wp:image {"lightbox":{"enabled":false},"width":"52px","height":"auto","scale":"cover","sizeSlug":"full","linkDestination":"none","className":"is-style-default on-hover-scale z-1000","style":{"border":{"radius":"999px","width":"2px"}},"borderColor":"base"} -->
-<figure class="wp-block-image size-full is-resized has-custom-border is-style-default on-hover-scale z-1000"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/placeholders/placeholder-avatar-1.webp" alt="" class="has-border-color has-base-border-color " style="border-width:2px;border-radius:999px;object-fit:cover;width:52px;height:auto"/></figure>
+<figure class="wp-block-image size-full is-resized has-custom-border is-style-default on-hover-scale z-1000"><img src="https://upa25.local/wp-content/themes/upa25/assets/images/placeholders/placeholder-avatar-1.webp" alt="" class="has-border-color has-base-border-color" style="border-width:2px;border-radius:999px;object-fit:cover;width:52px;height:auto"/></figure>
 <!-- /wp:image -->
 
 <!-- wp:image {"lightbox":{"enabled":false},"width":"52px","aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none","className":"is-style-default on-hover-scale z-2000","style":{"border":{"radius":"999px","width":"2px"},"spacing":{"margin":{"left":"-24px"}}},"borderColor":"base"} -->
-<figure class="wp-block-image size-full is-resized has-custom-border is-style-default on-hover-scale z-2000" style="margin-left:-24px"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/placeholders/placeholder-avatar-2.webp" alt="" class="has-border-color has-base-border-color " style="border-width:2px;border-radius:999px;aspect-ratio:1;object-fit:cover;width:52px"/></figure>
+<figure class="wp-block-image size-full is-resized has-custom-border is-style-default on-hover-scale z-2000" style="margin-left:-24px"><img src="https://upa25.local/wp-content/themes/upa25/assets/images/placeholders/placeholder-avatar-2.webp" alt="" class="has-border-color has-base-border-color" style="border-width:2px;border-radius:999px;aspect-ratio:1;object-fit:cover;width:52px"/></figure>
 <!-- /wp:image -->
 
 <!-- wp:image {"lightbox":{"enabled":false},"width":"52px","aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none","className":"is-style-default on-hover-scale z-3000","style":{"border":{"radius":"999px","width":"2px"},"spacing":{"margin":{"left":"-24px"}}},"borderColor":"base"} -->
-<figure class="wp-block-image size-full is-resized has-custom-border is-style-default on-hover-scale z-3000" style="margin-left:-24px"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/placeholders/placeholder-avatar-3.webp" alt="" class="has-border-color has-base-border-color " style="border-width:2px;border-radius:999px;aspect-ratio:1;object-fit:cover;width:52px"/></figure>
+<figure class="wp-block-image size-full is-resized has-custom-border is-style-default on-hover-scale z-3000" style="margin-left:-24px"><img src="https://upa25.local/wp-content/themes/upa25/assets/images/placeholders/placeholder-avatar-3.webp" alt="" class="has-border-color has-base-border-color" style="border-width:2px;border-radius:999px;aspect-ratio:1;object-fit:cover;width:52px"/></figure>
 <!-- /wp:image --></div>
 <!-- /wp:group -->
 
 <!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group"><!-- wp:paragraph {"fontSize":"small"} -->
-<p class="has-small-font-size"><?php /* Translators: 1. is the start of a 'mark' HTML element, 2. is the end of a 'mark' HTML element, 3. is the start of a 'strong' HTML element, 4. is the end of a 'strong' HTML element */
-echo sprintf( esc_html__( '%1$s★★★★★%2$s %3$s5,0%4$s', 'upa25' ), '<mark style="background-color:rgba(0, 0, 0, 0);color:#f8c761" class="has-inline-color">', '</mark>', '<strong>', '</strong>' ); ?></p>
+<p class="has-small-font-size"><mark style="background-color:rgba(0, 0, 0, 0);color:#f8c761" class="has-inline-color">★★★★★</mark> </p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph {"fontSize":"small"} -->
-<p class="has-small-font-size"><?php esc_html_e('Google-Bewertung', 'upa25');?></p>
+<p class="has-small-font-size"><strong>5,0</strong> Bewertung</p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->

e41f00f0bff1f69b3ee0580587cff69e5c5c15ad
hanneslsm <HannesLeismann@gmail.com>
2025-05-29 15:33:17 +0200

    Update card patterns

---
+++ patterns/cards/card-04.php
@@ -0,0 +1,130 @@
+<?php
+/**
+ * Title: Card-04
+ * Slug: upa25/card-04
+ * Categories: upa25/cards
+ * Viewport width: 1920
+ * Description: Layout section with cards
+ */
+?>
+<!-- wp:group {"tagName":"section","align":"full","className":"is-style-section-base-2","layout":{"type":"constrained"}} -->
+<section class="wp-block-group alignfull is-style-section-base-2"><!-- wp:group {"align":"wide","layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide"><!-- wp:spacer {"height":"var:preset|spacing|40"} -->
+<div style="height:var(--wp--preset--spacing--40)" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:group {"layout":{"type":"constrained","justifyContent":"left"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"className":"is-style-overline"} -->
+<p class="is-style-overline"><?php esc_html_e('This is your overline', 'upa25');?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"align":"wide"} -->
+<h2 class="wp-block-heading alignwide"><?php esc_html_e('Place your medium headline here.', 'upa25');?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><?php esc_html_e('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque id odio mollis, bibendum nibh eget, lacinia nibh. In molestie at dui et tincidunt.', 'upa25');?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"var:preset|spacing|20"} -->
+<div style="height:var(--wp--preset--spacing--20)" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"grid","minimumColumnWidth":"24rem"}} -->
+<div class="wp-block-group alignwide"><!-- wp:group {"className":"is-style-section-base","style":{"spacing":{"padding":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|20","right":"var:preset|spacing|20"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group is-style-section-base" style="padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--20)"><!-- wp:image {"width":"64px","aspectRatio":"1","scale":"cover","sizeSlug":"large","linkDestination":"none","className":"","style":{"border":{"radius":"999px"}}} -->
+<figure class="wp-block-image size-large is-resized has-custom-border"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/placeholders/placeholder-image-1.webp" alt="" style="border-radius:999px;aspect-ratio:1;object-fit:cover;width:64px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph -->
+<p><?php /* Translators: 1. is the start of a 'strong' HTML element, 2. is the end of a 'strong' HTML element */
+echo sprintf( esc_html__( '%1$sCurabitur Blandit Fringilla Sapien Eu.%2$s', 'upa25' ), '<strong>', '</strong>' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><?php esc_html_e('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Duis nec dolor ut elit laoreet vestibulum.', 'upa25');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"is-style-section-base","style":{"spacing":{"padding":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|20","right":"var:preset|spacing|20"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group is-style-section-base" style="padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--20)"><!-- wp:image {"width":"64px","aspectRatio":"1","scale":"cover","sizeSlug":"large","linkDestination":"none","className":"","style":{"border":{"radius":"999px"}}} -->
+<figure class="wp-block-image size-large is-resized has-custom-border"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/placeholders/placeholder-image-2.webp" alt="" style="border-radius:999px;aspect-ratio:1;object-fit:cover;width:64px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph -->
+<p><?php /* Translators: 1. is the start of a 'strong' HTML element, 2. is the end of a 'strong' HTML element */
+echo sprintf( esc_html__( '%1$sSuspendisse Potenti Curabitur Tempus.%2$s', 'upa25' ), '<strong>', '</strong>' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><?php esc_html_e('Phasellus ac justo ut orci sollicitudin tincidunt. Nullam aliquet, metus quis placerat finibus, justo nulla fermentum ipsum, at euismod erat augue in dolor. Integer ac augue ut arcu fringilla ultricies.', 'upa25');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"is-style-section-base","style":{"spacing":{"padding":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|20","right":"var:preset|spacing|20"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group is-style-section-base" style="padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--20)"><!-- wp:image {"width":"64px","aspectRatio":"1","scale":"cover","sizeSlug":"large","linkDestination":"none","className":"","style":{"border":{"radius":"999px"}}} -->
+<figure class="wp-block-image size-large is-resized has-custom-border"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/placeholders/placeholder-image-3.webp" alt="" style="border-radius:999px;aspect-ratio:1;object-fit:cover;width:64px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph -->
+<p><?php /* Translators: 1. is the start of a 'strong' HTML element, 2. is the end of a 'strong' HTML element */
+echo sprintf( esc_html__( '%1$sNunc Volutpat Ligula Dapibus Sem.%2$s', 'upa25' ), '<strong>', '</strong>' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><?php esc_html_e('Donec ornare, lacus a convallis dictum, dolor magna iaculis est, in elementum ligula justo id risus. Vivamus sit amet bibendum est. Aliquam erat volutpat. Sed bibendum vehicula ligula, id tincidunt est tempor eget.', 'upa25');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"is-style-section-base","style":{"spacing":{"padding":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|20","right":"var:preset|spacing|20"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group is-style-section-base" style="padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--20)"><!-- wp:image {"width":"64px","aspectRatio":"1","scale":"cover","sizeSlug":"large","linkDestination":"none","className":"","style":{"border":{"radius":"999px"}}} -->
+<figure class="wp-block-image size-large is-resized has-custom-border"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/placeholders/placeholder-image-4.webp" alt="" style="border-radius:999px;aspect-ratio:1;object-fit:cover;width:64px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph -->
+<p><?php /* Translators: 1. is the start of a 'strong' HTML element, 2. is the end of a 'strong' HTML element */
+echo sprintf( esc_html__( '%1$sPellentesque Vitae Odio Euismod Magna.%2$s', 'upa25' ), '<strong>', '</strong>' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><?php esc_html_e('Maecenas rhoncus massa nec nunc ultricies, non pharetra est tincidunt. Cras non libero a erat fringilla efficitur nec eget neque. Aenean cursus sapien vel urna ultrices, id congue ligula tristique.', 'upa25');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"is-style-section-base","style":{"spacing":{"padding":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|20","right":"var:preset|spacing|20"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group is-style-section-base" style="padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--20)"><!-- wp:image {"width":"64px","aspectRatio":"1","scale":"cover","sizeSlug":"large","linkDestination":"none","className":"","style":{"border":{"radius":"999px"}}} -->
+<figure class="wp-block-image size-large is-resized has-custom-border"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/placeholders/placeholder-image-5.webp" alt="" style="border-radius:999px;aspect-ratio:1;object-fit:cover;width:64px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph -->
+<p><?php /* Translators: 1. is the start of a 'strong' HTML element, 2. is the end of a 'strong' HTML element */
+echo sprintf( esc_html__( '%1$sEum fuga porro vel natus%2$s.', 'upa25' ), '<strong>', '</strong>' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><?php esc_html_e('Aut obcaecati veritatis eum odio laborum quo tenetur suscipit. Id eius perferendis rem deleniti perspiciatis et maxime dolores est dolores inventore id facilis veniam et quisquam illo?', 'upa25');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"is-style-section-base","style":{"spacing":{"padding":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|20","right":"var:preset|spacing|20"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group is-style-section-base" style="padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--20)"><!-- wp:image {"width":"64px","aspectRatio":"1","scale":"cover","sizeSlug":"large","linkDestination":"none","className":"","style":{"border":{"radius":"999px"}}} -->
+<figure class="wp-block-image size-large is-resized has-custom-border"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/placeholders/placeholder-image-6.webp" alt="" style="border-radius:999px;aspect-ratio:1;object-fit:cover;width:64px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph -->
+<p><?php /* Translators: 1. is the start of a 'strong' HTML element, 2. is the end of a 'strong' HTML element */
+echo sprintf( esc_html__( '%1$sEt amet optio id minima maxime.%2$s', 'upa25' ), '<strong>', '</strong>' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><?php esc_html_e('Qui quisquam rerum qui nostrum officia sit dolorum minima. Id porro fugit est eveniet neque aut eius aperiam ut molestiae reiciendis. Est officia nihil ut cumque ullam est amet nisi id natus velit.', 'upa25');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:spacer {"height":"var:preset|spacing|40"} -->
+<div style="height:var(--wp--preset--spacing--40)" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:group --></section>
+<!-- /wp:group -->

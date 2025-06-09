f03c65923362e4b111103526d0b82d7fe4c34492
hanneslsm <HannesLeismann@gmail.com>
2025-06-09 16:28:20 +0200

    Add new components

---
+++ patterns/components/component-reviews-02.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Title: component-reviews-02
+ * Slug: upa25/component-reviews-02
+ * Categories: upa25/components
+ * Viewport width: 320
+ */
+?>
+<!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size">Rated 4.5 out of 5 ★★★★★</p>
+<!-- /wp:paragraph -->

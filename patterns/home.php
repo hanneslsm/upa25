d98e7ddbe3f63ee542b091201e99529978674044
hanneslsm <HannesLeismann@gmail.com>
2025-07-30 16:02:05 +0200

    Remove overline

--- patterns/home.php
+++ patterns/home.php
@@ -11,12 +11,8 @@
 
 <!-- wp:group {"align":"wide","className":"is-style-section-brand-5","style":{"spacing":{"padding":{"bottom":"var:preset|spacing|40","top":"var:preset|spacing|50"}}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group alignwide is-style-section-brand-5" style="padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--40)"><!-- wp:group {"align":"wide","layout":{"type":"default"}} -->
-<div class="wp-block-group alignwide"><!-- wp:paragraph {"className":"is-style-overline"} -->
-<p class="is-style-overline"><?php esc_html_e('Bleibe immer auf dem neusten Stand', 'upa25');?></p>
-<!-- /wp:paragraph -->
-
-<!-- wp:heading {"level":1} -->
-<h1 class="wp-block-heading"><?php esc_html_e('Unsere Neuigkeiten', 'upa25');?></h1>
+<div class="wp-block-group alignwide"><!-- wp:heading {"level":1} -->
+<h1 class="wp-block-heading"><?php esc_html_e('Neuigkeiten', 'upa25');?></h1>
 <!-- /wp:heading --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group --></div>
@@ -46,15 +42,15 @@
 
 <!-- wp:query {"queryId":0,"query":{"perPage":3,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"exclude","inherit":false,"parents":[],"format":[]},"layout":{"type":"default"}} -->
 <div class="wp-block-query"><!-- wp:post-template {"layout":{"type":"default","columnCount":1}} -->
-<!-- wp:columns {"className":"is-style-section-base-2","style":{"spacing":{"blockGap":{"left":"0"}}}} -->
-<div class="wp-block-columns is-style-section-base-2"><!-- wp:column {"width":"33.33%","className":"with-display-none-when-empty"} -->
-<div class="wp-block-column with-display-none-when-empty" style="flex-basis:33.33%"><!-- wp:post-featured-image {"isLink":true,"aspectRatio":"3/2","width":"100%","height":"100%","style":{"layout":{"selfStretch":"fit","flexSize":null}}} /--></div>
-<!-- /wp:column -->
-
-<!-- wp:column {"verticalAlignment":"center","width":"","style":{"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30","right":"var:preset|spacing|30"}}}} -->
-<div class="wp-block-column is-vertically-aligned-center" style="padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)"><!-- wp:post-title {"level":3,"isLink":true,"style":{"elements":{"link":{"color":{"text":"var:preset|color|contrast-3"}}}},"textColor":"contrast-3","fontSize":"x-large"} /-->
+<!-- wp:columns {"className":"is-style-section-base","style":{"border":{"width":"1px"},"shadow":"var:preset|shadow|small"},"borderColor":"brand-4"} -->
+<div class="wp-block-columns is-style-section-base has-border-color has-brand-4-border-color" style="border-width:1px;box-shadow:var(--wp--preset--shadow--small)"><!-- wp:column {"verticalAlignment":"center","width":"","style":{"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30","right":"var:preset|spacing|30"}}}} -->
+<div class="wp-block-column is-vertically-aligned-center" style="padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)"><!-- wp:post-title {"level":3,"isLink":true,"style":{"elements":{"link":{"color":{"text":"var:preset|color|contrast-3"}}},"typography":{"textDecoration":"none"}},"textColor":"contrast-3","fontSize":"xx-large"} /-->
 
 <!-- wp:post-excerpt {"showMoreOnNewLine":false,"excerptLength":100} /--></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"33.33%","className":"with-display-none-when-empty"} -->
+<div class="wp-block-column with-display-none-when-empty" style="flex-basis:33.33%"><!-- wp:post-featured-image {"isLink":true,"aspectRatio":"3/2","width":"100%","height":"100%","style":{"layout":{"selfStretch":"fit","flexSize":null}}} /--></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns -->
 <!-- /wp:post-template -->

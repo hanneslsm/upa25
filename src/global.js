862cb47aba55454dc7bc7bdb7a205386742f4dc6
hanneslsm <HannesLeismann@gmail.com>
2025-12-09 09:26:04 +0100

    refactor: restructure header and footer components

---
+++ src/global.js
@@ -0,0 +1,12 @@
+/**
+ * global.js is loaded for both frontend and editor.
+ */
+
+
+
+/* Blocks are getting imported by Webpack automatically */
+
+/* Parts */
+import './parts/header-fixed/header-fixed.js';
+
+console.log( 'Global script loaded.' );
--- src/js/global.js
+++
@@ -1,7 +0,0 @@
-/**
- * global.js is loaded for both frontend and editor.
- */
-
-/* Import Scripts */
-import './scripts/header-fixed.js';
-import './scripts/sticker.js';

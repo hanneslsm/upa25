5b9201aa072e4ff86e7c123563931d1d38e02f94
hanneslsm <HannesLeismann@gmail.com>
2026-04-05 11:21:39 +0200

    feat: disable default password change notification email to reduce unnecessary notifications

--- inc/setup.php
+++ inc/setup.php
@@ -17,3 +17,19 @@ if (!function_exists('upa25_setup')) :
 	}
 endif; // upa25_setup
 add_action('after_setup_theme', 'upa25_setup');
+
+
+
+/**
+ * Remove the default password change notification email.
+ *
+ * Disables the email notification that WordPress sends to users when their password is changed.
+ * This is typically used to reduce email notifications or when custom password change
+ * notifications are handled by other means.
+ *
+ * @link https://developer.wordpress.org/plugins/hooks/actions/
+ * @since 0.4.4
+ */
+if ( !function_exists( 'wp_password_change_notification' ) ) {
+    function wp_password_change_notification() {}
+}
